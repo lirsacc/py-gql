d314db6cb089f1b0875806fb7b6459769e3f46ca
lirsacc <c.lirsac@gmail.com>
2018-06-12 20:15:03 +0200

    Clean up setup.py and package information

--- py_gql/__version__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-VERSION = (0, 0, 1)
-__version__ = ".".join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -7,53 +7,59 @@ from importlib import import_module
 from setuptools import setup, find_packages
 
 
-PKG_NAME = 'py_gql'
-PKG_AUTHOR = 'Charles Lirsac'
-PKG_AUTHOR_EMAIL = 'c.lirsac@gmail.com'
-PKG_URL = ''
+PKG_NAME = "py_gql"
+PKG_AUTHOR = "Charles Lirsac"
+PKG_AUTHOR_EMAIL = "c.lirsac@gmail.com"
+PKG_URL = ""
+
+VERSION = (0, 0, 1)
+__version__ = ".".join(map(str, VERSION))
 
 
 def run_setup():
-    with open('README.md') as f:
-        readme = '\n' + f.read()
+    with open("README.md") as f:
+        readme = "\n" + f.read()
 
-    with open('LICENSE') as f:
+    with open("LICENSE") as f:
         license_ = f.read()
 
-    _requirements = '''
+    _requirements = """
     six >= 1.11.0
     futures >= 3.1.1
-    '''
+    """
 
     requirements = [
-        line for line in (line.strip() for line in _requirements.split('\n'))
-        if line and not line.startswith('#')
+        line
+        for line in (line.strip() for line in _requirements.split("\n"))
+        if line and not line.startswith("#")
     ]
 
-    version = import_module('%s.__version__' % PKG_NAME).__version__
+    version = ".".join(map(str, VERSION))
 
     setup(
         name=PKG_NAME,
         version=version,
-        description=__doc__.split('\n')[0],
+        description=__doc__.split("\n")[0],
         long_description=readme,
         author=PKG_AUTHOR,
         author_email=PKG_AUTHOR_EMAIL,
         # url='',
         license=license_,
-        packages=find_packages(exclude=('tests', 'docs')),
+        packages=find_packages(exclude=("tests", "docs")),
         install_requires=requirements,
         include_package_data=True,
         classifiers=[
-            # Trove classifiers
-            # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: Implementation :: CPython',
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Libraries",
         ],
         # entry_points={
         #     'console_scripts': ['mycli=mymodule:cli'],
@@ -61,5 +67,5 @@ def run_setup():
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_setup()
