0e8fa41aedbb03a5f4588f1c5f31be16dcb6512f
lirsacc <c.lirsac@gmail.com>
2018-07-04 13:35:35 +0200

    lint fix

--- tests/test_execution/test_middleware.py
+++ tests/test_execution/test_middleware.py
@@ -3,7 +3,6 @@
 import pytest
 
 from py_gql.execution.middleware import apply_middlewares, GraphQLMiddleware
-from py_gql.execution import ThreadPoolExecutor
 from py_gql._graphql import graphql
 
 
