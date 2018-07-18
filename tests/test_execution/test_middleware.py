3677b29b8577fc01fe388bf7e778f4c918852f2f
lirsacc <c.lirsac@gmail.com>
2018-07-18 12:24:14 +0200

    Remove non two deterministic tests

--- tests/test_execution/test_middleware.py
+++ tests/test_execution/test_middleware.py
@@ -5,6 +5,7 @@ import pytest
 from py_gql._graphql import graphql
 from py_gql._string_utils import stringify_path
 from py_gql.execution.middleware import GraphQLMiddleware, apply_middlewares
+from py_gql.execution import ThreadPoolExecutor
 
 from ._test_utils import TESTED_EXECUTORS
 
@@ -92,22 +93,15 @@ def test_function_middleware(exe_cls, exe_kwargs, starwars_schema):
     ]
 
 
-@pytest.mark.parametrize("exe_cls, exe_kwargs", TESTED_EXECUTORS)
-def test_generator_middleware(exe_cls, exe_kwargs, starwars_schema):
+def test_generator_middleware(starwars_schema):
     log = []
 
-    def path_collector_one_way(next_, root, args, context, info):
+    def path_collector(next_, root, args, context, info):
         log.append("> %s" % stringify_path(info.path))
         yield next_(root, args, context, info)
         log.append("< %s" % stringify_path(info.path))
 
-    with exe_cls(**exe_kwargs) as executor:
-        graphql(
-            starwars_schema,
-            HERO_QUERY,
-            middlewares=[path_collector_one_way],
-            executor=executor,
-        )
+    graphql(starwars_schema, HERO_QUERY, middlewares=[path_collector])
 
     assert log == [
         "> hero",
@@ -127,6 +121,42 @@ def test_generator_middleware(exe_cls, exe_kwargs, starwars_schema):
     ]
 
 
+def test_generator_middleware_with_threads(starwars_schema):
+    log = set()
+
+    def path_collector(next_, root, args, context, info):
+        log.add("> %s" % stringify_path(info.path))
+        yield next_(root, args, context, info)
+        log.add("< %s" % stringify_path(info.path))
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        graphql(
+            starwars_schema,
+            HERO_QUERY,
+            middlewares=[path_collector],
+            executor=executor,
+        )
+
+    assert log == set(
+        [
+            "> hero",
+            "> hero.id",
+            "< hero.id",
+            "> hero.name",
+            "< hero.name",
+            "> hero.friends",
+            "> hero.friends[0].name",
+            "< hero.friends[0].name",
+            "> hero.friends[1].name",
+            "< hero.friends[1].name",
+            "> hero.friends[2].name",
+            "< hero.friends[2].name",
+            "< hero.friends",
+            "< hero",
+        ]
+    )
+
+
 class PathCollectorMiddleware(GraphQLMiddleware):
     def __init__(self):
         self.log = []
@@ -137,16 +167,9 @@ class PathCollectorMiddleware(GraphQLMiddleware):
         self.log.append("< %s" % stringify_path(info.path))
 
 
-@pytest.mark.parametrize("exe_cls, exe_kwargs", TESTED_EXECUTORS)
-def test_class_based_middleware(exe_cls, exe_kwargs, starwars_schema):
+def test_class_based_middleware(starwars_schema):
     path_collector = PathCollectorMiddleware()
-    with exe_cls(**exe_kwargs) as executor:
-        graphql(
-            starwars_schema,
-            HERO_QUERY,
-            middlewares=[path_collector],
-            executor=executor,
-        )
+    graphql(starwars_schema, HERO_QUERY, middlewares=[path_collector])
 
     assert path_collector.log == [
         "> hero",
