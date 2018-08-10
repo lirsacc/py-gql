8862f8045121d1d7462d056f1f02ad16cd6cde41
lirsacc <c.lirsac@gmail.com>
2018-08-10 12:39:07 +0200

    Include pygments lexer in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@
 import imp
 import os
 
-from docs import graphql_pygments
-
 # -- Project information -----------------------------------------------------
 
 about = imp.load_source("about", os.path.join("..", "py_gql", "__version__.py"))
@@ -208,3 +206,42 @@ autoclass_content = "both"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# -- Pygments custom lexer ---------------------------------------------------
+from pygments.lexer import RegexLexer
+from pygments import token
+from sphinx.highlighting import lexers
+
+
+class GraphqlLexer(RegexLexer):
+
+    name = "GraphQL"
+    aliases = ["graphql", "gql"]
+    filenames = ["*.graphql", "*.gql"]
+    mimetypes = ["application/graphql"]
+
+    tokens = {
+        "root": [
+            (r"#.*", token.Comment.Singline),
+            (r"\.\.\.", token.Operator),
+            (r'"[\u0009\u000A\u000D\u0020-\uFFFF]*"', token.String.Double),
+            (
+                r"(-?0|-?[1-9][0-9]*)"
+                r"(\.[0-9]+[eE][+-]?[0-9]+|\.[0-9]+|[eE][+-]?[0-9]+)",
+                token.Number.Float,
+            ),
+            (r"(-?0|-?[1-9][0-9]*)", token.Number.Integer),
+            (r"\$+[_A-Za-z][_0-9A-Za-z]*", token.Name.Variable),
+            (r"[_A-Za-z][_0-9A-Za-z]+\s?:", token.Text),
+            (
+                r"(type|query|mutation|@[a-z]+|on|true|false|null)\b",
+                token.Keyword.Type,
+            ),
+            (r"[!$():=@\[\]{|}]+?", token.Punctuation),
+            (r"[_A-Za-z][_0-9A-Za-z]*", token.Keyword),
+            (r"(\s|,)", token.Text),
+        ]
+    }
+
+
+lexers["graphql"] = GraphqlLexer(startinline=True)
