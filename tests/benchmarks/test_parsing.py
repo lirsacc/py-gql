13b9fc53cd866377a0f1e2fcdd9036dd81762f02
lirsacc <c.lirsac@gmail.com>
2019-05-01 10:49:33 +0100

    Fix schema parsing benchmarks

--- tests/benchmarks/test_parsing.py
+++ tests/benchmarks/test_parsing.py
@@ -4,6 +4,10 @@ from py_gql.lang import parse
 from py_gql.utilities import introspection_query
 
 
+def parse_schema(document):
+    return parse(document, allow_type_system=True)
+
+
 def test_parse_kitchen_sink(benchmark, fixture_file):
     doc = fixture_file("kitchen-sink.graphql")
     benchmark(parse, doc)
@@ -11,12 +15,12 @@ def test_parse_kitchen_sink(benchmark, fixture_file):
 
 def test_parse_schema_kitchen_sink(benchmark, fixture_file):
     doc = fixture_file("schema-kitchen-sink.graphql")
-    benchmark(parse, doc)
+    benchmark(parse_schema, doc)
 
 
 def test_parse_github_schema(benchmark, fixture_file):
     doc = fixture_file("github-schema.graphql")
-    benchmark(parse, doc)
+    benchmark(parse_schema, doc)
 
 
 def test_parse_introspection_query(benchmark):
